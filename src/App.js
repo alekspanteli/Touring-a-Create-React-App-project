de4fa3043679f32c1c91885a94ffe1da98688ab1
Alex Panteli <alekspanteli@gmail.com>
2022-07-25 16:40:30 +0300

    use Reducer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import './App.scss';
-import { useState, useEffect } from 'react';
+import { useReducer } from 'react';
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("tired");
-  
-  useEffect(() => {
-    console.log(`It's ${emotion} right now`)
-  }, [emotion]);
-
-  useEffect(() => {
-    console.log(`It's ${secondary} around here`)
-  }, [secondary])
-
+ 
+  const [checked, setChecked] = useReducer((checked) => !checked, false);
   return (
     <div className="App">
-      <h1>Current emotion is {emotion}</h1>
-      <button onClick={() => setEmotion("sad")}>Sad</button>
-      <button onClick={() => setEmotion("exited")}>Exited</button>
-      <button onClick={() => setEmotion("angry")}>Angry</button>
-      <h2>Current secondary emotion is {secondary}</h2>
-      <button onClick={() => setSecondary("grateful")}>Grateful</button>
+      <input type="checkbox" 
+      value={checked} 
+      onChange={setChecked} 
+      />
+      <label>{checked ? "checked" : "not checked"}</label>
     </div>
   );
 }
