e2a79ef7146de24f50e50628dfe8de14b8eb6bd3
Alex Panteli <alekspanteli@gmail.com>
2022-07-25 16:23:00 +0300

    add useeffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+  
+  useEffect(() => {
+    console.log(`It's ${emotion} right now`)
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`It's ${secondary} around here`)
+  }, [secondary])
+
   return (
     <div className="App">
       <h1>Current emotion is {emotion}</h1>
       <button onClick={() => setEmotion("sad")}>Sad</button>
       <button onClick={() => setEmotion("exited")}>Exited</button>
       <button onClick={() => setEmotion("angry")}>Angry</button>
+      <h2>Current secondary emotion is {secondary}</h2>
+      <button onClick={() => setSecondary("grateful")}>Grateful</button>
     </div>
   );
 }
