0ed2a6808fdeb5485152c745233e1df2f1c7b7b0
Alex Panteli <alekspanteli@gmail.com>
2022-07-31 16:03:45 +0300

    install react router

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
 import "./App.scss";
 
-const items = [
-  { name: "Feel", elevation: 2000 },
-  { name: "Everest", elevation: 2400 },
-  { name: "Tost", elevation: 300 },
-];
-
-function List({ data, renderItem, renderEmpty }) {
-  return !data.length ? (
-    renderEmpty
-  ) : (
-    <ul>
-      {data.map((item) => (
-        <li key={item.name}>{renderItem(item)}</li>
-      ))}
-    </ul>
-  );
-}
-
 function App() {
-  return (
-    <List
-      data={items}
-      renderEmpty={<p>This list is empty</p>}
-      renderItem={(item) => (
-        <>
-          {item.name} - {item.elevation} ft.
-        </>
-      )}
-    />
-  );
+  return <h1>App</h1>;
 }
 
 export default App;
