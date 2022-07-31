a9885fec326f9a2a95783fdba8debb7756b6725c
Alex Panteli <alekspanteli@gmail.com>
2022-07-31 16:41:30 +0300

    create nested link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import "./App.scss";
+import { Link, Outlet } from "react-router-dom";
 
-function App() {
-  return <h1>App</h1>;
-}
+export const Home = () => {
+  return (
+    <div>
+      <nav>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/">Home</Link>
+      </nav>
+      <h1>My website</h1>
+    </div>
+  );
+};
 
-export default App;
+export const About = () => {
+  return (
+    <div>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/">Home</Link>
+      <h1>About Us</h1>
+      <Outlet />
+    </div>
+  );
+};
+
+export const Contact = () => {
+  return (
+    <div>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/">Home</Link>
+      <h1>Contact Us</h1>
+    </div>
+  );
+};
+
+export const History = () => {
+  return (
+    <div>
+      <h1>Our history</h1>
+    </div>
+  );
+};
+
+export const App = () => {
+  return <Home />;
+};
